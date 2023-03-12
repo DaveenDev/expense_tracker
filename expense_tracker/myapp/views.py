67296dbde163fa2fa2d32706b0b29787b08c109c
DaveenDev <daveendev07@gmail.com>
2023-03-12 12:44:28 +0800

    total expenses

--- expense_tracker/myapp/views.py
+++ expense_tracker/myapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import redirect, render
+from django.db.models import Sum
 from .forms import ExpenseForm
 from .models import Expense
-
+import datetime
 # Create your views here.
+
+
+def calculate_days_expense(no_of_days):
+    expenses = Expense.objects.all()
+
+    daterange = datetime.date.today() - datetime.timedelta(days=no_of_days)
+    range_expenses = Expense.objects.filter(date__gt=daterange)
+    total= range_expenses.aggregate(Sum('amount'))
+    return total
 
 def index(request):
     
@@ -10,12 +20,22 @@ def index(request):
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
-
-    expense_form = ExpenseForm()
+    
     expenses = Expense.objects.all()
+    expense_form = ExpenseForm()
+    total_expenses = expenses.aggregate(Sum('amount'))
+
+    last_year_expenses = calculate_days_expense(365)
+    past_month_expenses = calculate_days_expense(30)
+    past_week_expenses= calculate_days_expense(7)
+    
     return render(request, "myapp/index.html", {
         'expense_form': expense_form,
-        'expenses': expenses
+        'expenses': expenses,
+        'total_expenses': total_expenses,
+        'last_year_expenses': last_year_expenses,
+        'past_month_expenses': past_month_expenses,
+        'past_week_expenses': past_week_expenses
     })
 
 
